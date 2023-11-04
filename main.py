3c739482cd67f70d98cb81b7ec95aefce6152ee4
SAint7579 <vishwa20@outlook.com>
2023-11-04 20:51:44 +0100

    File download API ready

--- main.py
+++ main.py
@@ -32,25 +32,35 @@ def process_image(input_file: UploadFile):
     processed_image_path = os.path.join(UPLOAD_DIR, f"{name}_rgba.png")
 
     # Call the Python scripts using subprocess
-    subprocess.run(["python", "dreamgaussian/process.py", f"dreamgaussian/data/{input_file.filename}"])
-    subprocess.run(["python", "dreamgaussian/main.py", "--config", "dreamgaussian/configs/image.yaml", "input=" + processed_image_path, f"save_path={name}", "force_cuda_rast=True"])
-    subprocess.run(["python", "dreamgaussian/main2.py", "--config", "dreamgaussian/configs/image.yaml", "input=" + processed_image_path, f"save_path={name}", "force_cuda_rast=True"])
+    # subprocess.run(["python", "dreamgaussian/process.py", f"dreamgaussian/data/{input_file.filename}"])
+    # subprocess.run(["python", "dreamgaussian/main.py", "--config", "dreamgaussian/configs/image.yaml", "input=" + processed_image_path, f"save_path={name}", "force_cuda_rast=True"])
+    # subprocess.run(["python", "dreamgaussian/main2.py", "--config", "dreamgaussian/configs/image.yaml", "input=" + processed_image_path, f"save_path={name}", "force_cuda_rast=True"])
 
     # Save the video using kiui.render
     # video_save_command = f"kiui.render logs/{name}.obj --save_video {name}.mp4 --wogui --force_cuda_rast"
     # subprocess.run(["python", "-m", video_save_command], shell=True)
     os.system(f"python -m kiui.render logs/{name}.obj --save_video output/{name}.gif --wogui --force_cuda_rast")
+    ## Move png, mtl and obj file to a new folder name
+    os.makedirs(f'logs/{name}', exist_ok=True)
+    shutil.move(f'logs/{name}.obj', f'logs/{name}/{name}.obj')
+    shutil.move(f'logs/{name}.mtl', f'logs/{name}/{name}.mtl')
+    shutil.move(f'logs/{name}_albedo.png', f'logs/{name}/{name}_albedo.png')
 
-    gif_path = f'output/{name}.gif'
+    # Saving the obj, mtl and png files into a zip file
+    shutil.make_archive(f'output/{name}', 'zip', f'logs/{name}')
+    # Remove the logs/name folder
+    shutil.rmtree(f'logs/{name}')
 
-    # Return the gif path
-    return gif_path
+    # Add gif path and zip path to a json format
+    json = {"gif_path": f'output/{name}.gif', "zip_path": f'output/{name}.zip'}
+
+    # Return the json
+    return json
 
 # Function to process text using process_text.py
 def process_text(input_text):
-    save_path = input_text.replace(" ", "_")
     ## Remove all special characters from the save path
-    save_path = "".join(e for e in save_path if e.isalnum())
+    save_path = "".join(e for e in save_path if e.isalnum()).lower()
     # Replace this with the actual command to process the text
     # For example, you can use subprocess to run your Python script
     subprocess.run(["python", "dreamgaussian/main.py", "--config", "dreamgaussian/configs/text_mv.yaml", "prompt=" + input_text, f"save_path={save_path}", "force_cuda_rast=True"])
@@ -61,20 +71,26 @@ def process_text(input_text):
 
     # Input and output file paths
     gif_path = f'output/{save_path}.gif'
+
+    # Saving the obj, mtl and png files into a zip file
+    shutil.make_archive(f'output/{save_path}', 'zip', f'logs/{save_path}')
     
-    # Return the gif path
-    return gif_path
+    # Add gif path and zip path to a json format
+    json = {"gif_path": gif_path, "zip_path": f'output/{save_path}.zip'}
+
+    # Return the json
+    return json
 
 # Route to handle image uploads
 @app.post("/upload-image/")
 async def upload_image(image: UploadFile):
     # Process the image
     try:
-        gif = process_image(image)
+        path = process_image(image)
 
         # Return the processed GIF
         # return FileResponse(output_file_path)
-        return FileResponse(gif)
+        return path
 
     except Exception as e:
         raise HTTPException(status_code=500, detail=f"Failed to process image: {str(e)}")
@@ -86,15 +102,29 @@ async def process_text_endpoint(text: str = Form(...)):
     # Define the output GIF file path
     try:
         # Process the text
-        gif = process_text(text)
+        path = process_text(text)
         # Return the processed GIF
-        return FileResponse(gif)
+        return path
     
     except Exception as e:
         raise HTTPException(status_code=500, detail=f"Failed to process text: {str(e)}")
 
+@app.post("/get-file/")
+async def process_text_endpoint(file_path: str = Form(...)):
 
-
+    # Getting the file name
+    file_name = file_path.split('/')[-1]
+    
+    # Define the output GIF file path
+    try:
+        # Return the processed GIF
+        return FileResponse(file_path, media_type='application/octet-stream', filename=file_name)
+    
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"Failed to process text: {str(e)}")
+    
+    
+    
 if __name__ == "__main__":
     import uvicorn
     uvicorn.run(app, host="0.0.0.0", port=8000)
