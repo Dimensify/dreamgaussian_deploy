621aade9f5a88f0470fd74cab1f6c03db6a08d7e
SAint7579 <vishwa20@outlook.com>
2023-11-12 16:36:36 +0100

    Bug fix in the GPU .sh code

--- run_servers.sh
+++ run_servers.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #Works on a 4 GPU machine
 
-CUDA_VISIBLE_DEVICES=0 uvicorn app1_module:app --host 0.0.0.0 --port 8000 &
-CUDA_VISIBLE_DEVICES=1 uvicorn app2_module:app --host 0.0.0.0 --port 8001 &
-CUDA_VISIBLE_DEVICES=2 uvicorn app3_module:app --host 0.0.0.0 --port 8002 &
-CUDA_VISIBLE_DEVICES=3 uvicorn app4_module:app --host 0.0.0.0 --port 8003 &
+CUDA_VISIBLE_DEVICES=0 uvicorn main:app --host 0.0.0.0 --port 8000 &
+CUDA_VISIBLE_DEVICES=1 uvicorn main:app --host 0.0.0.0 --port 8001 &
+CUDA_VISIBLE_DEVICES=2 uvicorn main:app --host 0.0.0.0 --port 8002 &
+CUDA_VISIBLE_DEVICES=3 uvicorn main:app --host 0.0.0.0 --port 8003 &
