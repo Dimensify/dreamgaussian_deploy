Added classifier function and fixed rotation bug

script/ten_second_functions.py
@@ -16,14 +16,14 @@ import bpy
 # from run import *
 
 ## Fetching the model and sampler globally to avoid reload
-print("### LOADING MVDREAM ###")
-model = build_model("sd-v2.1-base-4view")
-device = 'cuda' if torch.cuda.is_available() else 'cpu'
-model.device = device
-model.to(device)
-model.eval()
-sampler = DDIMSampler(model)
-uc = model.get_learned_conditioning( [""] ).to(device)
+# print("### LOADING MVDREAM ###")
+# model = build_model("sd-v2.1-base-4view")
+# device = 'cuda' if torch.cuda.is_available() else 'cpu'
+# model.device = device
+# model.to(device)
+# model.eval()
+# sampler = DDIMSampler(model)
+# uc = model.get_learned_conditioning( [""] ).to(device)
 
 ### Creating CRM Pipeline
 # print("### LOADING CRM ###")
@@ -295,11 +295,19 @@ def text_to_3d(prompt, obj_name, method='tripo'):
     str
         Path to the GIF file
     '''
-    global model, device, sampler, uc
+    # global model, device, sampler, uc
+    print("### LOADING MVDREAM ###")
+    model = build_model("sd-v2.1-base-4view")
+    device = 'cuda' if torch.cuda.is_available() else 'cpu'
+    model.device = device
+    model.to(device)
+    model.eval()
+    sampler = DDIMSampler(model)
+    uc = model.get_learned_conditioning( [""] ).to(device)
     dtype = torch.float16
     camera = None
     batch_size = 1
-    prompt = prompt + '.3D model, White background, symmetric, front facing.'
+    prompt = prompt + '.miniature 3D model, White background, symmetric, front facing.'
 
     img = t2i(model, 256, prompt, uc, sampler, step=100, scale=10, batch_size=batch_size, ddim_eta=0.0, 
             dtype=dtype, device=device, camera=camera, num_frames=4)